b254ee6c9f67b173d1713dc69e7c5526876ef31b
AlanKostrick <31017085+AlanKostrick@users.noreply.github.com>
2018-01-02 15:52:05 -0500

    Create TestSupport.java

---
+++ sorting/test/sorting/TestSupport.java
@@ -0,0 +1,34 @@
+package sorting;
+
+/**
+ * Ignore this stuff. It makes the other stuff work.
+ */
+public class TestSupport {
+
+	Class<?> _____;
+	
+	int __;
+	
+	boolean ___;
+
+	String ____ = "__";
+	
+	Object ______ = new Object() {
+
+		@Override
+		public int hashCode() {
+			throw new AssertionError();
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			throw new AssertionError();
+		}
+
+		@Override
+		public String toString() {
+			throw new AssertionError();
+		}
+		
+	};
+}
